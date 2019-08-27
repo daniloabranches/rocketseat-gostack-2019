f24ee52b546f0d23a061fd192f589646edb1a629
Danilo Abranches <daniloeabranches@gmail.com>
2019-08-27 08:08:09 -0300

    ESLint, Prettier e EditorConfig

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "OK" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'OK' });
 });
 
 export default routes;

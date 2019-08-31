a2daa5b49cd02116f2c1d200094e66e12679a38e
Danilo Abranches <daniloeabranches@gmail.com>
2019-08-31 08:40:57 -0300

    Criando loader de models

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'OK' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Danilo Abranches',
+    email: 'daniloeabranches@gmail.com',
+    password_hash: '000000000',
+  });
+
+  return res.json(user);
 });
 
 export default routes;

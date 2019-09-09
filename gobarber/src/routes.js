52d7b6a872cd0d366a0694b16a4c47ab888934bb
Danilo Abranches <daniloeabranches@gmail.com>
2019-09-09 08:24:27 -0300

    Listagem de prestadores de serviço

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,6 +19,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

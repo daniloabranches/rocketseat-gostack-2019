172e8b761fbc9f94bde5d0a791a22743c573a101
Danilo Abranches <daniloeabranches@gmail.com>
2019-09-11 06:47:44 -0300

    Listando agenda do prestador

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -10,6 +10,7 @@ import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -26,6 +27,8 @@ routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

0caa745b70905a227b658294aa2ca1f6eeeda624
Danilo Abranches <daniloeabranches@gmail.com>
2019-09-16 08:22:10 -0300

    Marcar notificações como lidas

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -26,12 +26,12 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
-
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

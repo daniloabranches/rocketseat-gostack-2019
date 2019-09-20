f9bfe358ab24f814ced6c92a6fa7eaf816810847
Danilo Abranches <daniloeabranches@gmail.com>
2019-09-20 07:53:20 -0300

    Configurando templates de e-mail

--- gobarber/src/lib/Main.js
+++ gobarber/src/lib/Main.js
@@ -1,4 +1,7 @@
 import nodemailer from 'nodemailer';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import { resolve } from 'path';
 
 import mailConfig from '../config/mail';
 
@@ -12,6 +15,27 @@ class Mail {
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+    const extName = '.hbs';
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: extName,
+        }),
+        viewPath,
+        extName,
+      })
+    );
   }
 
   sendMail(message) {

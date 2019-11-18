0990bfedb0785786a771ae2e93c609fff19861c5
Danilo Abranches <daniloeabranches@gmail.com>
2019-11-18 20:01:13 -0300

    Listando horários disponíveis

---
+++ gobarber/src/app/controllers/AvailableController.js
@@ -0,0 +1,58 @@
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
+
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+
+class AvailableController {
+  async index(req, res) {
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
+    }
+
+    const searchDate = Number(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    const schedule = ['20:00', '21:00', '22:00', '23:00'];
+
+    const avalaible = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        avalaible:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(avalaible);
+  }
+}
+
+export default new AvailableController();

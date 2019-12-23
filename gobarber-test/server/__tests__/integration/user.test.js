08431c97e3c72320644cdba39aa31946805da7fc
Danilo Abranches <daniloeabranches@gmail.com>
2019-12-23 07:26:33 -0300

    Teste de E-mail duplicado

--- gobarber-test/server/__tests__/integration/user.test.js
+++ gobarber-test/server/__tests__/integration/user.test.js
@@ -2,7 +2,13 @@ import request from 'supertest';
 
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('deve conseguir registrar', async () => {
     const response = await request(app)
       .post('/users')
@@ -13,5 +19,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('não deve conseguir registrar email duplicado', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Danilo',
+        email: 'teste@gmail.com',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Danilo',
+        email: 'teste@gmail.com',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });

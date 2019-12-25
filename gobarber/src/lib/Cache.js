a87b47b66db6a0749638474897894b0e523d1b25
Danilo Abranches <daniloeabranches@gmail.com>
2019-12-25 08:36:19 -0300

    Estrutura de cache

---
+++ gobarber/src/lib/Cache.js
@@ -0,0 +1,33 @@
+import Redis from 'ioredis';
+
+class Cache {
+  constructor() {
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      keyPrefix: 'cache:',
+    });
+  }
+
+  set(key, value) {
+    const expirationDateSeconds = 60 * 60 * 24;
+    return this.redis.set(
+      key,
+      JSON.stringify(value),
+      'EX',
+      expirationDateSeconds
+    );
+  }
+
+  async get(key) {
+    const cached = await this.redis.get(key);
+
+    return cached ? JSON.parse(cached) : null;
+  }
+
+  invalidate(key) {
+    this.redis.del(key);
+  }
+}
+
+export default new Cache();

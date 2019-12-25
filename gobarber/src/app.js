ed1ee6cdaba4e096f9ff7b1cb5202ddf377962b9
Danilo Abranches <daniloeabranches@gmail.com>
2019-12-25 09:40:44 -0300

    Utilizando Rate Limit

--- gobarber/src/app.js
+++ gobarber/src/app.js
@@ -6,6 +6,10 @@ import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import helmet from 'helmet';
 
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
+
 import 'express-async-errors';
 
 import routes from './routes';
@@ -31,10 +35,26 @@ class App {
     this.server.use(helmet());
 
     this.server.use(express.json());
+
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 3,
+        })
+      );
+    }
   }
 
   routes() {
